1bb1e06e0d3ff53abefb199d2c36fd9572f40756
simsforpolk <victor.sims@simsforpolk.com>
2025-07-18 22:19:04 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import Subscribe from './pages/Subscribe';
+
+function App() {
+  return (
+    <Router>
+      <div style={{ padding: 20, fontFamily: 'sans-serif' }}>
+        <h1>Activate Now</h1>
+        <nav style={{ marginBottom: 20 }}>
+          <Link to='/' style={{ marginRight: 10 }}>Home</Link>
+          <Link to='/events' style={{ marginRight: 10 }}>Events</Link>
+          <Link to='/subscribe'>Subscribe</Link>
+        </nav>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/events' element={<Events />} />
+          <Route path='/subscribe' element={<Subscribe />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Welcome to Activate Now</h2>
+      <p>Mobilizing our community for the future.</p>
+    </div>
+  );
+}
